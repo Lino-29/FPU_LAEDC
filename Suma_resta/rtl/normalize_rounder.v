6cf6f1f9fa15da03dc5a89ead4a0d7f9b8be0cc9
Lino-29 <lino_voxter@hotmail.com>
2025-06-23 17:17:25 -0600

    Correcion conversion sv --> v

--- Suma_resta/rtl/normalize_rounder.v
+++ Suma_resta/rtl/normalize_rounder.v
@@ -38,40 +38,38 @@ module normalize_rounder #(parameter WIDTH = 32) (
     wire [2:0] GRS;
     wire first_bit;
     wire [26:0] rounded_mant;
+    reg lz_error;  // Cambiado de 'bit' a 'reg'
 
     assign rounded_mant = (carry_out & op) ? ({1'b1,result_mant} >> 1) : result_mant;
     assign {first_bit,mant,GRS} = rounded_mant;
-    // Normalization
 
-    // always @(posedge clk) begin
-    bit lz_error;
     always @(*) begin
         lz_error = 0;
         if (first_bit == 0) begin
             casex (mant)
-                    {0'b0,1'b1,22'bx}: shift=1;
-                    {1'b0,1'b1,21'bx}: shift=2;
-                    {2'b0,1'b1,20'bx}: shift=3;
-                    {3'b0,1'b1,19'bx}: shift=4;
-                    {4'b0,1'b1,18'bx}: shift=5;
-                    {5'b0,1'b1,17'bx}: shift=6;
-                    {6'b0,1'b1,16'bx}: shift=7;
-                    {7'b0,1'b1,15'bx}: shift=8;
-                    {8'b0,1'b1,14'bx}: shift=9;
-                    {9'b0,1'b1,13'bx}: shift=10;
-                    {10'b0,1'b1,12'bx}: shift=11;
-                    {11'b0,1'b1,11'bx}: shift=12;
-                    {12'b0,1'b1,10'bx}: shift=13;
-                    {13'b0,1'b1,9'bx}: shift=14;
-                    {14'b0,1'b1,8'bx}: shift=15;
-                    {15'b0,1'b1,7'bx}: shift=16;
-                    {16'b0,1'b1,6'bx}: shift=17;
-                    {17'b0,1'b1,5'bx}: shift=18;
-                    {18'b0,1'b1,4'bx}: shift=19;
-                    {19'b0,1'b1,3'bx}: shift=20;
-                    {20'b0,1'b1,2'bx}: shift=21;
-                    {21'b0,1'b1,1'bx}: shift=22;
-                    {22'b0,1'b1,0'bx}: shift=23;
+                23'b00000000000000000000001: shift=23;
+                23'b0000000000000000000001x: shift=22;
+                23'b000000000000000000001xx: shift=21;
+                23'b00000000000000000001xxx: shift=20;
+                23'b0000000000000000001xxxx: shift=19;
+                23'b000000000000000001xxxxx: shift=18;
+                23'b00000000000000001xxxxxx: shift=17;
+                23'b0000000000000001xxxxxxx: shift=16;
+                23'b000000000000001xxxxxxxx: shift=15;
+                23'b00000000000001xxxxxxxxx: shift=14;
+                23'b0000000000001xxxxxxxxxx: shift=13;
+                23'b000000000001xxxxxxxxxxx: shift=12;
+                23'b00000000001xxxxxxxxxxxx: shift=11;
+                23'b0000000001xxxxxxxxxxxxx: shift=10;
+                23'b000000001xxxxxxxxxxxxxx: shift=9;
+                23'b00000001xxxxxxxxxxxxxxx: shift=8;
+                23'b0000001xxxxxxxxxxxxxxxx: shift=7;
+                23'b000001xxxxxxxxxxxxxxxxx: shift=6;
+                23'b00001xxxxxxxxxxxxxxxxxx: shift=5;
+                23'b0001xxxxxxxxxxxxxxxxxxx: shift=4;
+                23'b001xxxxxxxxxxxxxxxxxxxx: shift=3;
+                23'b01xxxxxxxxxxxxxxxxxxxxx: shift=2;
+                23'b1xxxxxxxxxxxxxxxxxxxxx: shift=1;
                 default: begin
                     shift=0;
                     lz_error = 1;
@@ -92,4 +90,3 @@ module normalize_rounder #(parameter WIDTH = 32) (
     end
 
 endmodule
-
