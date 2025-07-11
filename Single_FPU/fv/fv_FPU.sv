9dfd16bacddd011fe4f6816b312557238f515d29
DanielGG50 <daniel@email.com>
2025-07-11 21:11:30 +0000

    Now jaspergold can be used for this design

---
+++ Single_FPU/fv/fv_FPU.sv
@@ -0,0 +1,21 @@
+module fv_FPU(
+ 	input logic A,
+  input logic B,
+	input logic [1:0] op,
+  input logic clk,
+  input logic arst,
+  input logic en,
+  input logic [4:0] flags,
+  input logic [31:0] Z
+);
+
+	`define AST(block=fifo, name=no_name, precond=1'b1 |->, consq=1'b0) \
+	``block``_ast_``name``: assert property (@(posedge clk) disable iff(!arst_n) ``precond`` ``consq``);
+	
+	`define ASM(block=fifo, name=no_name, precond=1'b1 |->, consq=1'b0) \
+	``block``_asm_``name``: assume property (@(posedge clk) disable iff(!arst_n) ``precond`` ``consq``);
+
+	`define COV(block=fifo, name=no_name, precond=1'b1 |->, consq=1'b0) \
+	``block``_cov_``name``: cover property (@(posedge clk) disable iff(!arst_n) ``precond`` ``consq``);   
+
+endmodule
---
+++ Single_FPU/fv/jg_fpv.tcl
@@ -0,0 +1,46 @@
+clear -all
+
+#
+
+# Flags
+analyze -sv ../rtl/flags_block.v
+
+# Exceptions
+analyze -sv ../rtl/exception_block.v
+
+# Mul Div
+analyze -sv ../rtl/rounding.v
+analyze -sv ../rtl/normalizer.v
+analyze -sv ../rtl/mantissa_divider.v
+analyze -sv ../rtl/sum1b.v
+analyze -sv ../rtl/mul1b.v
+analyze -sv ../rtl/step.v
+analyze -sv ../rtl/flipflop.v
+analyze -sv ../rtl/shift.v
+analyze -sv ../rtl/parallel_shift.v
+analyze -sv ../rtl/mul.v
+analyze -sv ../rtl/exponent_logic.v
+analyze -sv ../rtl/sign_logic_muldiv.v
+analyze -sv ../rtl/mul_div.v
+ 
+# Add Sub
+analyze -sv ../rtl/normalize_rounder.v
+analyze -sv ../rtl/mantissa_add_sub.v
+analyze -sv ../rtl/mantissa_shifter.v 
+analyze -sv ../rtl/exponent_sub_upd.v
+analyze -sv ../rtl/sign_logic.v
+analyze -sv ../rtl/add_sub_main.v
+
+# Top
+analyze -sv ../rtl/FPU.v
+
+# FV
+analyze -sv fv_FPU.sv
+
+#
+#elaborate -bbox_a 65535 -bbox_mul 65535 -top FPU
+#
+#clock clk
+#
+#reset -expression arst
+#set_engineJ_max_trace_length 2000
