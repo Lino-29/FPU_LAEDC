25cf251c7b81283cdf1a900ff1d6f8554245febc
ChutsE <jesusesparza242@gmail.com>
2025-07-17 17:31:22 -0600

    changing add_sub folder name

--- Suma_resta/rtl/global_params.v
+++
@@ -1,5 +0,0 @@
-package global_params;
-    parameter int MANT_BITS = 23;
-    parameter int EXP_BITS   = 8;  // Bits del exponente
-    parameter int BIAS       = 127; // Sesgo del exponente para formato simple
-endpackage
